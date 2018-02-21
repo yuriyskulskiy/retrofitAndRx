23c9dfbbf498ec3ac299b3790ef1525fd77bfe05
yuriyskulskiy <yuriyskul@gmail.com>
2018-02-21 15:17:04 +0200

    refactor

--- app/src/main/java/example/yuriy/com/LoginViewModel.java
+++ app/src/main/java/example/yuriy/com/LoginViewModel.java
@@ -3,6 +3,9 @@ package example.yuriy.com;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
 
@@ -11,7 +14,6 @@ import example.yuriy.com.api.LoginResponse;
 import example.yuriy.com.api.QuestService;
 import example.yuriy.com.api.ServiceGenerator;
 import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
@@ -19,18 +21,18 @@ import io.reactivex.schedulers.Schedulers;
 import static example.yuriy.com.MainActivity.TOKEN;
 import static example.yuriy.com.MainActivity.USER_ID;
 
-/**
- * Created by yuriy on 2/20/2018.
- */
+
 
 public class LoginViewModel extends ViewModel {
-
-    boolean isSubscribed = true;
 
     private Observable<LoginResponse> mLoginResponseObservable;
     private Disposable disposable;
 
     private MutableLiveData<LiveDataLoginContainer> loadedLoginData;
+
+    public static LoginViewModel getViewModel(@NonNull FragmentActivity activity) {
+        return ViewModelProviders.of(activity).get(LoginViewModel.class);
+    }
 
     public LoginViewModel() {
         super();
@@ -46,36 +48,36 @@ public class LoginViewModel extends ViewModel {
         return loadedLoginData;
     }
 
+    public void clearLifeData() {
+        loadedLoginData.setValue(null);
+        safeDispose(disposable);
+    }
+
     public void runSubscription() {
         safeDispose(disposable);
 
         disposable = mLoginResponseObservable.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+//                .observeOn(AndroidSchedulers.mainThread())  //todo remove it?
                 .subscribeWith(new DisposableObserver<LoginResponse>() {
                     @Override
                     public void onNext(LoginResponse value) {
                         Log.e("RXT", "onNext ");
-                        loadedLoginData.setValue(new LiveDataLoginContainer(value, null));
+                        loadedLoginData.postValue(new LiveDataLoginContainer(value, null));
                     }
 
                     @Override
                     public void onError(Throwable e) {
                         Log.e("RXT", "onError ");
-                        loadedLoginData.setValue(new LiveDataLoginContainer(null, e));
+                        loadedLoginData.postValue(new LiveDataLoginContainer(null, e));
                     }
 
                     @Override
                     public void onComplete() {
-                        //do nothing
                         Log.e("RXT", "onComplete ");
                     }
                 });
     }
 
-    public void clear() {
-        loadedLoginData = new MutableLiveData<LiveDataLoginContainer>();  //   new empty live data needs new callback to refresh ui
-        safeDispose(disposable);
-    }
 
     private void safeDispose(Disposable disposable) {
         if (disposable != null && !disposable.isDisposed()) {
--- app/src/main/java/example/yuriy/com/MainActivity.java
+++ app/src/main/java/example/yuriy/com/MainActivity.java
@@ -1,70 +1,65 @@
 package example.yuriy.com;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import example.yuriy.com.api.ErrorHandler;
 import example.yuriy.com.api.LoginResponse;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    public static final String TOKEN = "EAACm5v3mKgwBAGxoNtncAAGX9caoi3jOAk5SJQ4zkpROOoc3qonKCNIrZANoZAobKferxwrpKyGjnziw1PZAAzcmLYiISLd93cdCDU69AYtEnru075LGNO9zCceTeTYSUnbHirv1SInPSBalZBkfqs6Vz7uHQcDDg3NSZCY0ZCF3bu3i6nA2sbxL2tZBw7GFN8L3aK8vFVPQvXUNNLr4aLwgMDouAS1R5AZD";
-    public static final String USER_ID = "1624473360973123";
+    TextView infoTv;
+    public static final String TOKEN = "EAALq5VBw9AIBADKIZB9cajW0dtaOYxtgW7Qqu8aRG45uUztb6WsvrRfeTPCiHSOwFxwkzf7GeBqF0eforqTfyaQHl9wE8gNd03bjwgI1KVWHhLg2N4BZCTJlhMr7RoZBNeD9FsiJyhIEm0Ql5N36bhTpkAASuOeBNyFJuC6HUzzI8ZCbKKhCPbXDzeKPWDEXn9aHOXNusMGYGZCyBapxSnEpCcEfiYKAZD";
+    public static final String USER_ID = "1644826165604509";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-            subscribeToLiveData();
+        infoTv = findViewById(R.id.info);
+
+
+        subscribeToLiveData();
     }
 
     public void onLogin(View view) {
-        if(!getViewModel().isSubscribed){
-            subscribeToLiveData();
-        }
-        getViewModel().runSubscription();
+        LoginViewModel.getViewModel(MainActivity.this).runSubscription();
     }
 
     private void subscribeToLiveData() {
-        getViewModel().isSubscribed = true;
-        getViewModel().getLoginResponseLiveData().observe(this, new android.arch.lifecycle.Observer<LoginViewModel.LiveDataLoginContainer>() {
+        LoginViewModel.getViewModel(MainActivity.this).getLoginResponseLiveData().observe(this, new android.arch.lifecycle.Observer<LoginViewModel.LiveDataLoginContainer>() {
             @Override
             public void onChanged(@Nullable LoginViewModel.LiveDataLoginContainer liveDataLoginContainer) {
                 if (liveDataLoginContainer == null) {
                     Log.e("onChanged", "or first start or clearedData");
+                    return;
                 }
                 if (liveDataLoginContainer.isNoErrorResponse()) {
                     LoginResponse resultResp = liveDataLoginContainer.getLoginResponse();
                     String token = resultResp.getAuthToken();
                     boolean isNewUser = resultResp.isNewUser();
-                    Log.e("LiveData", "noError" + "token:" + token + " is new user = " + isNewUser);
+
+                    infoTv.setText(token);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.e("onChanged", "noError" + "token:" + token + " is new user = " + isNewUser);
                     //save to SharedPref, go to start activity, call finish()
                 } else {
 
                     ErrorHandler.logError(liveDataLoginContainer.getThrowable());
                 }
-
-                clearViewModel();
-                getViewModel().isSubscribed = false;
+                LoginViewModel.getViewModel(MainActivity.this).clearLifeData();
             }
         });
     }
-
-    public LoginViewModel getViewModel() {
-        return ViewModelProviders.of(this).get(LoginViewModel.class);
-    }
-
-    private void clearViewModel() {
-        getViewModel().clear();
-    }
-
-
-
 
 }
